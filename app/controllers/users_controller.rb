9ca1da439919aa7874c8354f3ef2692f33bfe6a5
NAGAADITHYAV <nagaadithyavuppalapati@gmail.com>
2020-09-03 16:55:08 +0530

    almost done except socila  media part

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
+    print current_user[:id]
+    puts "done, this is  what we are talking about"
+    #exit
   end
 end
